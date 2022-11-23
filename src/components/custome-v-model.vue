cace19c3b869f4f8f50efa3803f9e3281d43d1af
kannan <kannans@datasirpi.com>
2022-11-23 17:39:40 +0530

    note added

--- src/components/custome-v-model.vue
+++ src/components/custome-v-model.vue
@@ -1,10 +1,11 @@
+<!--what is customize v-model-- if 2 companies one is accentur another one is tcs they are want to input in differnt data and differnt style. then we will create a single customize-v-model-->
 <template>
   <Input type="text" :value="modelValue" @input="$emit('update:modelValue', $event.target.value)"/>
   <!--if the given input is does't store the data property we will define some changes,
-     when we use v-model in custome component it automatically receive prop call(modelvalue), 
-  we need to specify in the props option. 
-  next we need to bind the props to the value attribute of input element (:value="modelValue", this will be take care of value of the input)
-  when we use v-model directive in custome component it automaticaly listen to an event called 'update:modelValue',
+when we use v-model in custome component it automatically receive prop call(modelvalue), 
+we need to specify in the props option. 
+next we need to bind the props to the value attribute of input element (:value="modelValue", this will be take care of value of the input)
+when we use v-model directive in custome component it automaticaly listen to an event called 'update:modelValue',
 so the input element we bind to the input element and emit the update 'update:modelValue', then the data we passed to the parent is ($event.target.value)which is input element value-->
 </template>
 <script>
